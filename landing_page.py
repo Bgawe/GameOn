9d235e2505f09c2c6d344cc406e497ec0a0cf0f7
myugasds <135064231+Bgawe@users.noreply.github.com>
2024-11-07 00:43:17 +0700

    Add files via upload

---
+++ landing_page.py
@@ -0,0 +1,46 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import Screen
+from kivy.uix.image import Image
+from kivy.graphics import Color, Rectangle
+from kivy.clock import Clock
+
+class ImageButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(ImageButton, self).__init__(**kwargs)
+
+class LandingPage(Screen):
+    def __init__(self, **kwargs):
+        super(LandingPage, self).__init__(**kwargs)
+
+        self.layout = FloatLayout()
+
+        self.background = Image(source='backgroundLanding.png', allow_stretch=True, keep_ratio=False)
+        self.layout.add_widget(self.background)
+
+        with self.layout.canvas.before:
+            Color(1, 1, 1)  
+            self.rect = Rectangle(size=self.layout.size, pos=self.layout.pos)
+
+        self.layout.bind(size=self._update_rect, pos=self._update_rect)
+
+        # Menggunakan ImageButton
+        self.start_button = ImageButton(
+            source='images/button_landing.png',  # Ganti dengan path gambar tombol yang diinginkan
+            size_hint=(None, None),
+            size=(450, 120),
+            pos_hint={'center_x': 0.52, 'center_y': 0.25}
+        )
+        self.start_button.bind(on_press=self.go_to_menu)
+
+        self.layout.add_widget(self.start_button)
+
+        self.add_widget(self.layout)
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+
+    def go_to_menu(self, instance):
+        self.manager.current = 'menu'
